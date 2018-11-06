3f50d4a93154d3f1aa8ee2e71705d733ec628195
kevinfernaldy <kevin.fernaldy@gmail.com>
2018-11-06 13:04:28 +0700

    Added struct for payload and ack frame

--- utilities.h
+++ utilities.h
@@ -1,6 +1,24 @@
 #ifndef __UTILITIES_H__
 #define __UTILITIES_H__
 
+#define ASCII_SOH 0x01
+#define ASCII_ACK 0x06
+#define ASCII_NAK 0x15
+
+struct packet_payload {
+    char SOH;
+    int sequence_number;
+    int data_length;
+    char data[1024];
+    unsigned char checksum;
+};
+
+struct packet_ack {
+    char ACK;
+    int next_sequence_number;
+    unsigned char checksum;
+};
+
 int checkIfNumbers(char* param,int ip_flag);
 
 #endif
